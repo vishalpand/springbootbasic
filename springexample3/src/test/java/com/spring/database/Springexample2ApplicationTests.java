739f8c5d2f7265eed69aad7dc1a7b6670d4f3fd8
vishalpand <106236306+vishalpand@users.noreply.github.com>
2022-05-26 14:37:15 +0530

    Add files via upload

---
+++ springexample3/src/test/java/com/spring/database/Springexample2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.database;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springexample2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
